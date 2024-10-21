4644723445a578da120aa51f0047f644892f6ada
Mateus <118356038+Matz2022@users.noreply.github.com>
2024-10-21 15:38:24 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -50,6 +50,32 @@ app.get("/produtos", async(req,res)=>{
     
    
 })
+
+
+// Nova rota para usuários
+app.get("/usuarios", async (req, res) => {
+    try {
+        const conexao = await mysql.createConnection({
+            host: process.env.dbhost ? process.env.dbhost : "localhost",
+            user: process.env.dbuser ? process.env.dbuser : "root",
+            password: process.env.dbpassword ? process.env.dbpassword : "",
+            database: process.env.dbname ? process.env.dbname : "banco1022b",
+            port: process.env.dbport ? parseInt(process.env.dbport) : 3306
+        });
+
+        // Query para selecionar usuários
+        const [result, fields] = await conexao.query("SELECT * FROM usuarios");
+        
+        await conexao.end();
+        res.send(result);
+    } catch (e) {
+        res.status(500).send("Erro do servidor");
+    }
+});
+
+
+
+
 //INICIAR o Servidor 
 app.listen(8000,()=>{
     console.log("SERVIDOR INICIADO NA PORTA 8000")
