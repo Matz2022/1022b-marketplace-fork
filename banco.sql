7392d7eaf286b3faffd2d958ab0b5ea9f8c48c32
Mateus <118356038+Matz2022@users.noreply.github.com>
2024-10-21 08:27:35 -0400

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -7,3 +7,15 @@ CREATE TABLE IF NOT EXISTS produtos(
     imagem VARCHAR(300)
 );
 INSERT INTO produtos VALUES (1,'Iphone','Celular RUIM',5000.50,'SEM IMAGEM');
+
+
+USE defaultdb;
+CREATE TABLE IF NOT EXISTS usuarios(
+id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50),
+    email VARCHAR(100),
+    created_at timestamp(4),
+    updated_at timestamp(4)
+);
+
+INSERT INTO `defaultdb`.`usuarios` (`id`, `nome`, `email`, `created_at`, `updated_at`) VALUES ('1', 'Mateus', 'mateus@gmail.com', '2024-10-21 07:40', '2024-11-22 07:41');
